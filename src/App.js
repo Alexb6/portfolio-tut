e343aaf4dfd5e34052700a007d70a5ab8eed1875
Alex Tran <info@alex-tran.com>
2019-03-11 00:43:03 +0100

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+class App extends Component {    
+
+    // constructor() {
+    //     super();
+    //     this.state = { displayBio: false };
+    //     // this.readMore = this.readMore.bind(this);
+    //     // this.showLess = this.showLess.bind(this);
+    //     this.toggleBioDisplay = this.toggleBioDisplay.bind(this);
+    // }
+
+    // readMore() {
+    //     this.setState({ displayBio: true });
+    // }
+
+    // showLess() {
+    //     console.log('showLess this ', this);
+    //     this.setState({ displayBio: false });
+    // }
+
+    // toggleBioDisplay() {
+    //     this.setState({ displayBio: !this.state.displayBio });
+    // }
+
+    state = { displayBio: false }; // This will be equivalent to the constructor, no need for the bind anymore
+
+    toggleBioDisplay = () => {
+        this.setState({ displayBio: !this.state.displayBio });
+    }
+
+    render() {
+        const bio =  (
+            <div>
+                <p>I live in Paris, and I code every day.</p>
+                <p>My favorite language is Javascript, and I think that React.js is awesome.</p>
+                <p>Besides coding, I also love graphic design ang photography.</p>
+                <button onClick = {this.toggleBioDisplay}>Read Less</button>
+            </div>
+        );
+
+        return (
+            <div>
+                <h1>Hello !</h1>
+                <p>My name is Alex Tran. I'm a Full Stack JS Web Developper.</p>
+                <p>I'm always looking forward to working on meaningful projects</p>
+                {this.state.displayBio ? bio : (
+                    <div>
+                        <button onClick = {this.toggleBioDisplay}>Read More</button>
+                    </div>
+                    )
+                }
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ src/data/projects.js
@@ -0,0 +1,11 @@
+import project1 from '../assets/project1.png';
+import project2 from '../assets/project2.png';
+import project3 from '../assets/project3.png';
+
+const PROJECT = [ { // screen case syntax
+    title: 'Example React Application',
+    description : 'A React App tjat I built, involving JS and core web dev concepts !',
+    link: 'https://github.com/15Dkatz/example',
+    image: project1
+}
+]; 
